f51cb35f991724b6a9657458573e4ce0297fe93b
khaled hossain <md.khaled.eee@gmail.com>
2022-03-05 17:26:29 +0600

    Add Memento Pattern Example

--- src/com/khaledHossainCode/Main.java
+++ src/com/khaledHossainCode/Main.java
@@ -1,13 +1,25 @@
 package com.khaledHossainCode;
 
+import com.khaledHossainCode.memento.Editor;
+import com.khaledHossainCode.memento.EditorHistory;
+
 public class Main {
 
     public static void main(String[] args) {
-        TaxCalculator calculator = getCalculator();
-        System.out.println(calculator.calculateTax());
+        var editor = new Editor();
+        var history = new EditorHistory();
+
+        editor.setContent("a");//typing something
+        history.push(editor.createState()); // saving it
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop()); //undo
+        editor.restore(history.pop()); //undo
+        System.out.println(editor.getContent()); // a
+
     }
 
-    public static TaxCalculator getCalculator() {
-        return new TaxCalculator2019();
-    }
 }
---
+++ src/com/khaledHossainCode/memento/Editor.java
@@ -0,0 +1,22 @@
+package com.khaledHossainCode.memento;
+
+public class Editor {
+    private String content;
+
+    // create state means press save button
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    public void restore(EditorState state) {
+        content = state.getContent();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
